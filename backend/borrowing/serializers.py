bd6e91d274c7b4e16628cce2643f5d26e587eb09
Ihor Borunov <borunov65@gmail.com>
2025-10-18 22:38:06 +0200

    Implement create a serializer and fix views.py

--- backend/borrowing/serializers.py
+++ backend/borrowing/serializers.py
@@ -31,3 +31,16 @@ class BorrowingListSerializer(serializers.ModelSerializer):
             "book",
             "user",
         ]
+
+
+class BorrowingCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = [
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
+            "user",
+        ]
--- backend/borrowing/views.py
+++ backend/borrowing/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets, mixins
 from .models import Borrowing
-from .serializers import BorrowingListSerializer
 from catalog.permissions import IsAdminOrReadOnly
+from .serializers import BorrowingListSerializer, BorrowingCreateSerializer
 
 
 class BorrowingViewSet(
@@ -13,5 +13,9 @@ class BorrowingViewSet(
     viewsets.GenericViewSet,
 ):
     queryset = Borrowing.objects.select_related("book", "user").all()
-    serializer_class = BorrowingListSerializer
     permission_classes = [IsAdminOrReadOnly]
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return BorrowingCreateSerializer
+        return BorrowingListSerializer
