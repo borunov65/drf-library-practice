46ae3ca8819ead13c70d97adcde398d2da5b9446
Ihor Borunov <borunov65@gmail.com>
2025-10-18 20:39:07 +0200

    Fix library/urls.py and add path in it

--- backend/library/urls.py
+++ backend/library/urls.py
@@ -26,8 +26,9 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/", include("catalog.urls", namespace="catalog")),
-    path("api/", include("user.urls", namespace="user")),
+    path("api/books/", include("catalog.urls", namespace="catalog")),
+    path("api/borrowings/", include("borrowing.urls", namespace="borrowing")),
+    path("api/users/", include("user.urls", namespace="user")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/doc/swagger/",
