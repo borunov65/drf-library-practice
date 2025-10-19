b7ca6e633da72dea2f46aac8b5913e462e551e30
Ihor Borunov <borunov65@gmail.com>
2025-10-19 22:06:10 +0200

    Add logic for borrowing accesses

--- backend/borrowing/views.py
+++ backend/borrowing/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets, mixins
+from rest_framework.permissions import IsAuthenticated
 from .models import Borrowing
-from catalog.permissions import IsAdminOrReadOnly
 from .serializers import BorrowingListSerializer, BorrowingCreateSerializer
 
 
@@ -11,9 +11,18 @@ class BorrowingViewSet(
     viewsets.GenericViewSet,
 ):
     queryset = Borrowing.objects.select_related("book", "user").all()
-    permission_classes = [IsAdminOrReadOnly]
+    permission_classes = [IsAuthenticated]
 
     def get_serializer_class(self):
         if self.action == "create":
             return BorrowingCreateSerializer
         return BorrowingListSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return Borrowing.objects.all()
+        return Borrowing.objects.filter(user=user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
